e7baa33c54dcd6958fba014a9ebe47aff39bca5f
Melody Lan <melody_lan@sfu.ca>
2020-02-15 12:40:11 -0800

    Init

---
+++ android/src/com/gamejam/helth/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.gamejam.helth;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.gamejam.helth.Helth;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Helth(), config);
+	}
+}

91207367f008cd36b244c3dd1ee4c42608a70912
Yutong Li <rainbowzom@gmail.com>
2020-02-15 17:06:54 -0800

    Incomplete main class!!!

---
+++ core/src/com/gamejam/helth/GameOverScreen.java
@@ -0,0 +1,118 @@
+package com.gamejam.helth;
+
+//package com.gamejam.helth;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+
+import sun.applet.Main;
+
+
+public class GameOverScreen implements Screen {
+
+
+
+    final Helth game;
+    Texture retryButtonTexture;
+    OrthographicCamera camera;
+    TextureRegion retryButtonTextureRegion;
+    TextureRegionDrawable retryButtonDrawable;
+    ImageButton retryButton;
+    int finalScore;
+
+    public GameOverScreen(final Helth game){
+        this.game = game;
+
+//
+//        retryButtonTexture = new Texture(Gdx.files.internal("button_retry.png"));
+//
+//        retryButtonTextureRegion = new TextureRegion(retryButtonTexture);
+//        retryButtonDrawable = new TextureRegionDrawable(retryButtonTextureRegion);
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 1280, 720);
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta){
+        Gdx.gl.glClearColor(0.8f, 0 ,0.2f,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+
+
+//        Gdx.gl.glClearColor(0, 0 ,0.2f,1);
+//        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+//        retryButton = new ImageButton(retryButtonDrawable);
+//        retryButton.setTransform(true);
+//        retryButton.setX(500);
+//        retryButton.setY(360);
+//
+//        retryButton.setSize(100, 100);
+         finalScore = 100;
+        // TODO: get finalscore
+        String scoreDisplay = "Your Final Score Is " + finalScore;
+        game.batch.begin();
+//        game.batch.begin();
+
+        game.font2.draw(game.batch, scoreDisplay, 475, 700);
+
+        game.font.draw(game.batch, "PRESS ANYWHERE TO RETRY", 570, 400);
+//        retryButton.draw(game.batch, 1);
+        game.batch.end();
+
+        if (Gdx.input.isTouched()){
+            game.setScreen(new MainMenuScreen(game));
+            //TODO : set it to GameScreen instead
+            dispose();
+        }
+//        playButton.addListener( new ClickListener() {
+//            @Override
+//            public void clicked(InputEvent event, float x, float y) {
+//                System.out.println("hiii");
+//                Assets.load();
+//                // game.getSoundManager().play( TyrianSound.CLICK );
+//                game.setScreen( new GameScreen(game) );
+//            }
+//        } );
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+
+}
---
+++ core/src/com/gamejam/helth/MainMenuScreen.java
@@ -0,0 +1,116 @@
+package com.gamejam.helth;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+
+
+public class MainMenuScreen implements Screen {
+
+
+
+
+    final Helth game;
+    float inputTime = 0;
+//    Texture playButtonTexture;
+//    TextureRegion playButtonTextureRegion;
+//    ImageButton playButton;
+//    TextureRegionDrawable playButtonDrawable;
+
+    OrthographicCamera camera;
+
+    public MainMenuScreen(final Helth game){
+        this.game = game;
+
+
+//        playButtonTexture = new Texture(Gdx.files.internal("playbutton.png"));
+//
+//        playButtonTextureRegion = new TextureRegion(playButtonTexture);
+//        playButtonDrawable = new TextureRegionDrawable(playButtonTextureRegion);
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 1280, 720);
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta){
+
+        Gdx.gl.glClearColor(0.2f, 0.6f ,0.2f,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+//         playButton = new ImageButton(playButtonDrawable);
+//        playButton.setTransform(true);
+//        playButton.setX(500);
+//        playButton.setY(360);
+//
+//        playButton.setSize(100, 100);
+        game.batch.begin();
+        game.font2.draw(game.batch, "HELTH", 750, 700);
+        game.font.draw(game.batch, "PRESS ANYWHERE TO START", 570, 400);
+
+
+        game.batch.end();
+        if (Gdx.input.isTouched()){
+            if (inputTime  == 0 || inputTime > 1) { //delay in seconds
+                game.setScreen(new GameOverScreen(game));
+                //TODO : set it to GameScreen instead
+                dispose();
+            }
+
+                inputTime += delta;
+
+        }
+        else {inputTime = 0;}
+
+
+//       playButton.addListener()
+
+
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+
+}

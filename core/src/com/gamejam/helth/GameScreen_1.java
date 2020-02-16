4ec35080039a5e21d06f99b0065e2b04ff22e61a
Melody Lan <melody_lan@sfu.ca>
2020-02-16 12:04:26 -0800

    Change character bounds

--- core/src/com/gamejam/helth/GameScreen_1.java
+++ core/src/com/gamejam/helth/GameScreen_1.java
@@ -134,7 +134,7 @@ public class GameScreen_1 implements Screen {
         game.batch.draw(vegetable.characterImage, vegetable.x, vegetable.y, vegetable.width, vegetable.height);
 
         if (enemy.collision(vegetable, 0f)){
-            vegetable.setHealth(0);
+            //vegetable.setHealth(0);
             vegetable.death();
         }
 
@@ -194,8 +194,8 @@ public class GameScreen_1 implements Screen {
             vegetable.x = 0;
         }
 
-        if (vegetable.x > 2075){
-            vegetable.x = 2075;
+        if (vegetable.x > 1500){
+            vegetable.x = 1500;
         }
 
 

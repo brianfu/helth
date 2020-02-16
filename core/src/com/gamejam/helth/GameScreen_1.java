e1417eefefac4243a5a5ac6d7bac4024bbce9d3b
Yutong Li <rainbowzom@gmail.com>
2020-02-16 11:51:11 -0800

    death sound

--- core/src/com/gamejam/helth/GameScreen_1.java
+++ core/src/com/gamejam/helth/GameScreen_1.java
@@ -34,7 +34,10 @@ public class GameScreen_1 implements Screen {
     GroundBlock floor;
     long lastDropTime;
     long lastShotFired;
+
     Music bgm;
+    Sound deathSound;
+
     //long lastEnemyDisappear;
     //int dropsGathered;
     TextureRegion backgroundTexture;
@@ -55,6 +58,8 @@ public class GameScreen_1 implements Screen {
         bgm.setLooping(true);
         bgm.setVolume(0.4f);
         bgm.play();
+
+        deathSound = Gdx.audio.newSound(Gdx.files.internal("death_sound.wav"));
         //backgroundTexture = new TextureRegion(new Texture("firstscreen.jpg"), 0, 0, 2220, 1080);
         //platform6 = new Texture("platform6.png");
         //platform3 = new Texture("platform3.png");
@@ -215,6 +220,7 @@ public class GameScreen_1 implements Screen {
 
         if (vegetable.isDead()) {
             game.setScreen(new EndScreen(game));
+            deathSound.play();
             dispose();
         }
 
